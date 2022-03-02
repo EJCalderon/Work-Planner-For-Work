83407f02976b08a8da12118d07db055b4af5308e
EJCalderon <emiliojaviercalderon@gmail.com>
2022-03-01 23:16:47 -0600

    made id's

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,14 +1,4 @@
 document.getElementById("currentDay").innerHTML = moment().format('dddd, MMM Do YYYY');
 
-var userInput8A = document.querySelector("#8");
-var userInput9A = document.querySelector("#9")
-var userInput10A = document.querySelector("#10");
-var userInput11A = document.querySelector("#11");
-var userInput12P = document.querySelector("#12");
-var userInput1P = document.querySelector("#1");
-var userInput2P = document.querySelector("#2");
-var userInput3P = document.querySelector("#3");
-var userInput4P = document.querySelector("#4");
-var userInput5P = document.querySelector("#5");
 
 
