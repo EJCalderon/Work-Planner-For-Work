36c6269be351a2f16a75da9d58b3670eeea9c361
EJCalderon <emiliojaviercalderon@gmail.com>
2022-03-01 23:35:50 -0600

    tried to add but cannot figure out syntax

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,10 @@
 document.getElementById("currentDay").innerHTML = moment().format('dddd, MMM Do YYYY');
 
+if(localStorage.getItem("8Tasks") !=""){
+    $("#8Am").val(localStorage.getItem("8Tasks"));
+}
 
-
+saveBtn8.addEventListener("click", function(){
+    var userInput = $("8Tasks");
+    localStorage.setItem("8Task", $("#8Am"))
+});
